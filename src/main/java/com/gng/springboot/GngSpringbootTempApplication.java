e9293879f22c613883e088eb1c3afc4332a29e41
Rurien1124 <a01092603909@gmail.com>
2022-03-21 13:15:34 +0900

    gng-springboot 최초 등록
    - 초기 설정 사용

---
+++ src/main/java/com/gng/springboot/GngSpringbootTempApplication.java
@@ -0,0 +1,13 @@
+package com.gng.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GngSpringbootTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GngSpringbootTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gng/springboot/GngSpringbootTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gng.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GngSpringbootTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
