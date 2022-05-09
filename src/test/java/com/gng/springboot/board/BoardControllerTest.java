a046a11ff6dff57b357ad87cfc34cd45d81b16dd
hyunmo.gu <gusah0909@naver.com>
2022-05-09 16:51:37 +0900

    BoardController 수정 - createBoard 함수 테스트

--- src/test/java/com/gng/springboot/board/BoardControllerTest.java
+++ src/test/java/com/gng/springboot/board/BoardControllerTest.java
@@ -1,10 +1,19 @@
 package com.gng.springboot.board;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
 
+import com.gng.springboot.board.model.BoardDto;
 import com.gng.springboot.board.repository.BoardRepository;
 import com.gng.springboot.board.service.BoardService;
 import com.gng.springboot.board.service.BoardServiceImpl;
@@ -14,6 +23,7 @@ import com.gng.springboot.commons.base.BaseControllerTest;
 public class BoardControllerTest extends BaseControllerTest{
 	@Autowired
 	private BoardService boardService;
+	
 	
 	@Autowired
 	private BoardRepository boardRepository;
@@ -25,13 +35,29 @@ public class BoardControllerTest extends BaseControllerTest{
 	private final static String name = "testName";
 	private final static String data = "집가고싶다";
 	
+	@Nested
 	@DisplayName("tdd 테스트")
 	public class BoardCreateTest {
-		@Test
+		
 		@DisplayName("create test")
+		@Test
 		public void createBoardTest() throws Exception {
 			// given
-//			final BoardDto boardDto = 
+			BoardDto boardDto = BoardDto.builder()
+					.gngAccountId(id)
+					.boardName(name)
+					.boardData(data)
+					.build();
+			
+			// when
+			mockMvc.perform(post("/board")
+					.contentType(MediaType.APPLICATION_JSON)
+					.accept(MediaTypes.HAL_JSON_VALUE)
+					.content(objectMapper.writeValueAsString(boardDto)))
+				.andDo(print())
+				.andExpect(status().isCreated())
+//				.andExpect(jsonPath("gngAccountId").exists())
+				;
 		}
 		
 	}
